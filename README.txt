74818aa6228d9a7cf5c9bf9825301ca98c979708
Mohammad Amin <alamin.cse.99@gmail.com>
2024-04-20 00:42:38 +0600

    deploy front and back

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
-Github Link==https://github.com/MohammadAlamin99/AyykoriDigitalLtdTask
+Github Link== https://github.com/MohammadAlamin99/contactMangement
 
-Live Link =  https://65f9b320ec83b80f0e241105--papaya-dusk-1a1855.netlify.app/ 
+Live Link =  https://6622ba87a81bb91841655163--elegant-pithivier-eb4a0e.netlify.app/ 
 
 
 
--- client/apiRequiest/apiRequiest.js
+++ client/apiRequiest/apiRequiest.js
@@ -6,7 +6,7 @@ let BaseURL = "http://localhost:5000";
 export  async function addContactRequest(name,email,phone,address,photo) {
     try {
         let reqBody ={name:name, email:email, phone:phone, address:address,photo:photo};
-        let result = await axios.post(BaseURL+'/api/v1/addContact',reqBody);
+        let result = await axios.post('https://contact-mangement-theta.vercel.app/api/v1/addContact',reqBody);
         let data =  result;
         return data;
     }
@@ -18,7 +18,7 @@ export  async function addContactRequest(name,email,phone,address,photo) {
 
  export async function allContactRequest() {
     try {
-        let result = await axios.get(BaseURL+'/api/v1/allContact');
+        let result = await axios.get('https://contact-mangement-theta.vercel.app/api/v1/allContact');
         let data =  result.data['data'];
         return data;
     }
@@ -30,7 +30,7 @@ export  async function addContactRequest(name,email,phone,address,photo) {
 
  export  async function deleteContactRequest(id) {
     try {
-        let result = await axios.get(BaseURL+'/api/v1/deleteCon/'+id);
+        let result = await axios.get('https://contact-mangement-theta.vercel.app/api/v1/deleteCon/'+id);
         let data = result
         return data;
     }
@@ -45,7 +45,7 @@ export  async function addContactRequest(name,email,phone,address,photo) {
  export  async function updateContactRequest(id,name,email,phone,address,photo) {
     try {
         let reqBody ={id:id,name:name, email:email, phone:phone, address:address,photo:photo};
-        let result = await axios.post(BaseURL+'/api/v1/updateCon',reqBody);
+        let result = await axios.post('https://contact-mangement-theta.vercel.app/api/v1/updateCon',reqBody);
         return result
     }
     catch (e) {
